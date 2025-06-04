c92ddfeb4210598789132bff362d936599cb212d
Gladin Danill <danyagladin@gmail.com>
2025-06-04 23:09:55 +0700

    :80

--- main.go
+++ main.go
@@ -126,8 +126,8 @@ func main() {
 	fmt.Println(err)
 	fmt.Println(os.Getenv("secret"))
 	http.HandleFunc("/webhook", handleWebhook)
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting server on :80")
+	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
 	}
 }
